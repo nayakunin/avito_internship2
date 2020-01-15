10cf6e1e116e4ba2575624e7057f986a6d11db61
nayakunin <nayakunin@outlook.com>
2020-01-15 21:45:33 +0300

    added markup and popup toggler mechanism

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import CardContainer from './components/CardContainer';
+import Popup from './components/Popup/Popup';
+
 
 function App() {
+
+  const [popupIsOpen, setPopupIsOpen] = useState(false);
+  const [popupContent, setPopupContent] = useState();
+  const [popup, setPopup] = useState(null);
+
+  const handlePopupOpen = (cardContent) => {
+    setPopupContent(cardContent);
+    setPopupIsOpen(true);
+  }
+
+  const handlePopupClose = () => {
+    setPopupIsOpen(false);
+  }
+
+  useEffect(() => {
+    if (popupIsOpen) {
+      setPopup(
+        <Popup
+          content={popupContent}
+          handlePopupClose={handlePopupClose}
+        />
+      )
+    } else {
+      setPopup(null);
+    }
+  }, [popupIsOpen, popupContent]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CardContainer handlePopupOpen={handlePopupOpen} />
+      {popup}
     </div>
   );
 }
