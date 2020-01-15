7775018703eda63f74cf2f1b7d6d05222dd80c21
nayakunin <nayakunin@outlook.com>
2020-01-16 00:40:57 +0300

    added styles for popup component

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Popup.css';
 import { ReactComponent as Close } from './Close.svg';
+import { ReactComponent as Arrow } from './Arrow.svg';
 
 function Popup(props) {
   const popupCloseBtn = React.createRef();
@@ -23,19 +24,18 @@ function Popup(props) {
     <div className="popup" onClick={handleClose} ref={popupWindow}>
       <div className="popup__content" ref={popupContent}>
         <Close className="popup__close-btn" onClick={handleClose} ref={popupCloseBtn} />
-        <div className="popup__header">
+        <div className="popup__text">
           <h2 className="popup__title">{popup.title}</h2>
-          <div className="popup__header-block_right">
-            <h3 className="popup__address">{popup.address}</h3>
-            <p className="popup__price">{popup.price}</p>
-          </div>
+          <h3 className="popup__address">{popup.address}</h3>
+          <p className="popup__price">{popup.price}</p>
+          <p className="popup__description">{popup.description}</p>
+          <p className="popup__seller-name">{popup.sellerName}</p>
         </div>
         <div className="popup__img-container">
-          <img src={popup.images[1]} alt="Изображение недвижимости" className="popup__img" />
-          <div className="img-container__btn_left"></div>
-          <div className="img-container__btn_right"></div>
+          <img src={popup.images[0]} alt="Изображение недвижимости" className="popup__img" />
+          <Arrow className="img-container__btn img-container__btn_left"/>
+          <Arrow className="img-container__btn img-container__btn_right"/>
         </div>
-        <p className="popup__description">{popup.description}<span className="popup__seller-name">{popup.sellerName}</span></p>
       </div>
     </div>
   )
