bb3e390b659e837ea53d2b81f34e925e7955f917
nayakunin <nayakunin@outlook.com>
2020-01-15 23:24:09 +0300

    added styles for desktop resolutions for card component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Card.css';
+import { ReactComponent as OpenMenu } from './open-menu.svg';
 
 function Card(props) {
 
@@ -12,13 +13,16 @@ function Card(props) {
   }
 
   return (
-    <div className="card" onClick={handleClick}>
-      <div className="card__image-placeholder">
+    <div className="card" >
+      <div className="card__image-placeholder card__block_left" onClick={handleClick}>
         <img src={props.url} alt="Изображение недвижимости" className="card__img"/>
       </div>
-      <h2 className="card__title">{props.title}</h2>
-      <p className="card__address">{props.address}</p>
-      <h3 className="card__price">{props.price}</h3>
+      <div className="card__block_right">
+        <h2 className="card__title">{props.title}</h2>
+        <p className="card__address">{props.address}</p>
+        <h3 className="card__price">{props.price}</h3>
+        <OpenMenu className="card__open-menu" onClick={handleClick}/>
+      </div>
     </div>
   )
 }
