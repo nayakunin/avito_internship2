1e238275df9cb1e0fb6643b54931c98f0eaed653
nayakunin <nayakunin@outlook.com>
2020-01-16 14:32:38 +0300

    Updates

---
+++ precache-manifest.8e2b26b659726ea3e7514125e59e65a4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a997e9729babb0cea743c374a2954b8",
+    "url": "/avito_internship2/index.html"
+  },
+  {
+    "revision": "9243ce956c5c04819a37",
+    "url": "/avito_internship2/static/css/main.9d0ed452.chunk.css"
+  },
+  {
+    "revision": "9777a32a833f5deb29a9",
+    "url": "/avito_internship2/static/js/2.fab4fb21.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/avito_internship2/static/js/2.fab4fb21.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9243ce956c5c04819a37",
+    "url": "/avito_internship2/static/js/main.577fd3d5.chunk.js"
+  },
+  {
+    "revision": "ce5192c366e8b99b8310",
+    "url": "/avito_internship2/static/js/runtime-main.a1b54c15.js"
+  },
+  {
+    "revision": "6b2049e3755170c75b03e465619bab08",
+    "url": "/avito_internship2/static/media/Arrow.6b2049e3.svg"
+  },
+  {
+    "revision": "6092516ea927337ac66e6f3000a87d4e",
+    "url": "/avito_internship2/static/media/Close.6092516e.svg"
+  },
+  {
+    "revision": "6b2049e3755170c75b03e465619bab08",
+    "url": "/avito_internship2/static/media/open-menu.6b2049e3.svg"
+  }
+]);
